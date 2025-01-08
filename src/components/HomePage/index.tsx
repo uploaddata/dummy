8e9aadad59eddd4d16f2748c9c4a542100eba43c
Chetan Bardoliya <workspace.chetan@gmail.com>
2025-01-08 17:14:25 +0530

    remove API from FE

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 
 const HomePage = () => {
-  const callApi = async () => {
-    try {
-      const response = await axios.get("/api");
-      console.log(response.data);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  };
+  //   const callApi = async () => {
+  //     try {
+  //       const response = await axios.get("/api");
+  //       console.log(response.data);
+  //       return response.data;
+  //     } catch (error) {
+  //       console.error(error);
+  //       return error;
+  //     }
+  //   };
 
-  useEffect(() => {
-    callApi();
-  });
+  //   useEffect(() => {
+  //     callApi();
+  //   });
   return <div>HomePage</div>;
 };
 
