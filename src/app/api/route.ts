9d311bff626985aea65510a714476accbd227d5e
Chetan Bardoliya <workspace.chetan@gmail.com>
2025-01-09 14:42:52 +0530

    API changes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server";
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const handleTime = async () => {
-  console.log("call func");
-  setTimeout(() => {
-    console.log("Inside TimeOut");
-  }, 100000);
+  console.log("Call func");
+  await delay(100000);
+  console.log("Inside Timeout");
 };
 
 export async function GET() {
-  console.log("call API");
+  console.log("Call API");
   await handleTime();
   return NextResponse.json({ status: "OK" });
 }
