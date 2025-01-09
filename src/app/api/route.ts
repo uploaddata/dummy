708a6f5135bb14f4030fd0f8d98804ec7df3e4b1
Chetan Bardoliya <workspace.chetan@gmail.com>
2025-01-09 15:00:26 +0530

    other code added

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,29 @@
 import { NextResponse } from "next/server";
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// const handleTime = async () => {
+//   console.log("call func");
+//   setTimeout(() => {
+//     console.log("Inside TimeOut");
+//   }, 100000);
+// };
 
-const handleTime = async () => {
+// export async function GET() {
+//   console.log("call API");
+//   await handleTime();
+//   return NextResponse.json({ status: "OK" });
+// }
+
+const handleTimeoput = async () => {
   console.log("Call func");
-  await delay(100000);
-  console.log("Inside Timeout");
+  return new Promise(() => {
+    setTimeout(() => {
+      console.log("Called every 100 seconds");
+    }, 100000); // 100 seconds in milliseconds
+  });
 };
 
 export async function GET() {
   console.log("Call API");
-  await handleTime();
+  await handleTimeoput();
   return NextResponse.json({ status: "OK" });
 }
